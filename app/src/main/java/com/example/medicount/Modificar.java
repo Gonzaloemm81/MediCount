597a1bc302ec932943e8376635a9b69fcb226837
Roberto <mirandaroberto1994@gmail.com>
2023-10-29 05:24:32 -0300

    agrega la opcion de eliminar medicamentos

--- app/src/main/java/com/example/medicount/Modificar.java
+++ app/src/main/java/com/example/medicount/Modificar.java
@@ -11,7 +11,7 @@ import android.widget.EditText;
 public class Modificar extends AppCompatActivity {
 
     EditText et_nombre;
-    Button bt_modificar;
+    Button bt_modificar, bt_eliminar;
     int id;
     String nombre;
 
@@ -20,6 +20,14 @@ public class Modificar extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_modificar);
         Bundle b= getIntent().getExtras();
+
+        Button btnBack = findViewById(R.id.btn_back);
+        btnBack.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                finish(); // Cierra la actividad actual
+            }
+        });
         if (b!=null){
             id = b.getInt("Id");
             nombre = b.getString("Nombre");
@@ -30,6 +38,15 @@ public class Modificar extends AppCompatActivity {
         et_nombre.setText(nombre);
 
         bt_modificar = (Button) findViewById(R.id.bt_modificar);
+        bt_eliminar = (Button) findViewById(R.id.bt_eliminar);
+
+        bt_eliminar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Eliminar(id);
+                onBackPressed();
+            }
+        });
 
         bt_modificar.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -45,6 +62,15 @@ public class Modificar extends AppCompatActivity {
         SQLiteDatabase db = helper.getWritableDatabase();
 
         String sql = "update Medicamentos set Nombre='" + Nombre + "' where Id="+Id;
+        db.execSQL(sql);
+        db.close();
+    }
+
+    private void Eliminar(int Id){
+        BaseHelper helper = new BaseHelper(this, "Demo",null,1);
+        SQLiteDatabase db = helper.getWritableDatabase();
+
+        String sql = "delete from Medicamentos where Id="+Id;
         db.execSQL(sql);
         db.close();
     }
