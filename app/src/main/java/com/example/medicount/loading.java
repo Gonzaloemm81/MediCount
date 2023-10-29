8cd9a2febc5e7d81bc3b23c7f4558f4bb2a2197d
Roberto <mirandaroberto1994@gmail.com>
2023-10-29 09:09:36 -0300

    crea loading

---
+++ app/src/main/java/com/example/medicount/loading.java
@@ -0,0 +1,18 @@
+package com.example.medicount;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+
+public class loading extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_loading);
+
+
+    }
+}
